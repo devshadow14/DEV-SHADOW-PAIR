77074386c87acbbe26db747b29af1c3e2d3a1e65
devshadow14 <nd172237@gmail.com>
2026-06-12 00:38:23 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+// config.js
+export default {
+    // Pr茅fixe du bot
+    PREFIX: "馃懢",
+    
+    // Nom du bot
+    BOT_NAME: "饾櫦饾櫚饾殎饾櫢饾櫚 饾殗饾櫝",
+    
+    // Num茅ros des owners (admin)
+    OWNERS: [
+        "24177474264@s.whatsapp.net", // Remplacez par votre num茅ro
+    ],
+    
+    // Param猫tres du bot
+    SETTINGS: {
+        ANTI_LIEN: false,
+        ANTI_PROMOTE: false,
+        WELCOME: true,
+        GOODBYE: true
+    },
+    
+    // Messages automatiques
+    AUTO_MESSAGES: {
+        WELCOME: "Bienvenue dans le groupe ! 馃帀",
+        GOODBYE: "Au revoir ! 馃憢"
+    }
+};
